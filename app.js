288e563a29851b5f9b0c4d0eb9379eb3fa50eed4
Sumanth Reddy Kanukuntla <sumanthkanukuntla16@gmail.com>
2022-11-02 18:42:26 -0500

    added all the functionality

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var shoppingRouter = require('./routes/shopping');
+var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -20,9 +22,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/shopping', shoppingRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
